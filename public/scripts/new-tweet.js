5e336f8cef0d471b342bc26370b0ee644191a482
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-03-12 20:11:10 -0700

    Change: function isTweetValid is created to chekc if the tweet is valid. Also the function implemented to form submission.

--- public/scripts/new-tweet.js
+++ public/scripts/new-tweet.js
@@ -1,7 +1,27 @@
+const isTweetValid = (valueOfTextarea) => {
+  if (valueOfTextarea.length > 140) {
+    alert("Your tweet is too long");
+    return false;
+  }
+
+  if (valueOfTextarea === null || valueOfTextarea === "") {
+    alert("You need to write something");
+    return false;
+  }
+
+  return true;
+};
+
 $(document).ready(() => {
   // POST for /api/tweets
   $("section.new-tweet > form").on("submit", function (event) {
     event.preventDefault();
+
+    const $text = $("#tweet-text").val();
+
+    if (!isTweetValid($text)) {
+      return;
+    }
 
     // fetching data to server with ajax
     $.ajax({
@@ -9,7 +29,7 @@ $(document).ready(() => {
       url: "/api/tweets",
       data: $(this).serialize(),
     }).catch((error) => {
-      alert("You need to write something");
+      console.log(error);
       // if (data.content.text === "" || data.content.text === null) {
       //   alert("You need to write something!");
       // }
